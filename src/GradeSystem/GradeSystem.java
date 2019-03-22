9e7e0b73229d3878600b056817eccef2049792d3
365460 <365460@gmail.com>
2019-03-22 13:55:57 +0800

    create

---
+++ src/GradeSystem/GradeSystem.java
@@ -0,0 +1,5 @@
+package GradeSystem;
+
+public class GradeSystem {
+
+}
---
+++ src/hw2/Main.java
@@ -0,0 +1,17 @@
+package hw2;
+import java.io.InputStream;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("---");
+		
+		while(true) {
+			String x = scanner.next();
+			System.out.println(x + "\n");
+		}
+	}
+}
